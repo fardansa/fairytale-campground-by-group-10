759c5356b101399414a82e7bcf2897bda3728776
fardan sakha <fardansa30@gmail.com>
2025-11-19 22:15:04 +0700

    smoga bnr databasenya

--- database/fairytale_campground.sql
+++ database/fairytale_campground.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 19, 2025 at 05:06 AM
+-- Generation Time: Nov 19, 2025 at 04:13 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -24,73 +24,13 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `camp`
---
-
-CREATE TABLE `camp` (
-  `camp_id` int(11) UNSIGNED NOT NULL,
-  `paket_id` int(11) UNSIGNED NOT NULL,
-  `nomor_camp` varchar(10) NOT NULL,
-  `nomor_loker` varchar(10) NOT NULL,
-  `status` enum('tersedia','tidak tersedia') NOT NULL DEFAULT 'tersedia'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `camp`
---
-
-INSERT INTO `camp` (`camp_id`, `paket_id`, `nomor_camp`, `nomor_loker`, `status`) VALUES
-(1, 1, 'S01', '1', 'tersedia'),
-(2, 1, 'S02', '2', 'tersedia'),
-(3, 1, 'S03', '3', 'tersedia'),
-(4, 1, 'S04', '4', 'tersedia'),
-(5, 1, 'S05', '5', 'tersedia'),
-(6, 1, 'S06', '6', 'tersedia'),
-(7, 1, 'S07', '7', 'tersedia'),
-(8, 1, 'S08', '8', 'tersedia'),
-(9, 1, 'S09', '9', 'tersedia'),
-(10, 1, 'S10', '10', 'tersedia'),
-(11, 2, 'D01', '11', 'tersedia'),
-(12, 2, 'D02', '12', 'tersedia'),
-(13, 2, 'D03', '13', 'tersedia'),
-(14, 2, 'D04', '14', 'tersedia'),
-(15, 2, 'D05', '15', 'tersedia'),
-(16, 2, 'D06', '16', 'tersedia'),
-(17, 2, 'D07', '17', 'tersedia'),
-(18, 2, 'D08', '18', 'tersedia'),
-(19, 2, 'D09', '19', 'tersedia'),
-(20, 2, 'D10', '20', 'tersedia'),
-(21, 3, 'F01', '21', 'tersedia'),
-(22, 3, 'F02', '22', 'tersedia'),
-(23, 3, 'F03', '23', 'tersedia'),
-(24, 3, 'F04', '24', 'tersedia'),
-(25, 3, 'F05', '25', 'tersedia'),
-(26, 3, 'F06', '26', 'tersedia'),
-(27, 3, 'F07', '27', 'tersedia'),
-(28, 3, 'F08', '28', 'tersedia'),
-(29, 3, 'F09', '29', 'tersedia'),
-(30, 3, 'F10', '30', 'tersedia'),
-(31, 3, 'F11', '31', 'tersedia'),
-(32, 3, 'F12', '32', 'tersedia'),
-(33, 3, 'F13', '33', 'tersedia'),
-(34, 3, 'F14', '34', 'tersedia'),
-(35, 3, 'F15', '35', 'tersedia'),
-(36, 3, 'F16', '36', 'tersedia'),
-(37, 3, 'F17', '37', 'tersedia'),
-(38, 3, 'F18', '38', 'tersedia'),
-(39, 3, 'F19', '39', 'tersedia'),
-(40, 3, 'F20', '40', 'tersedia');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `detail_pemesanan`
 --
 
 CREATE TABLE `detail_pemesanan` (
   `detail_id` int(10) UNSIGNED NOT NULL,
   `pemesanan_id` int(10) UNSIGNED NOT NULL,
-  `camp_id` int(11) UNSIGNED NOT NULL,
+  `tent_id` int(11) UNSIGNED NOT NULL,
   `harga_per_malam` int(10) NOT NULL,
   `subtotal` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -115,9 +55,9 @@ CREATE TABLE `paket` (
 --
 
 INSERT INTO `paket` (`paket_id`, `nama_paket`, `deskripsi`, `fasilitas`, `kapasitas`, `harga`) VALUES
-(1, 'single', 'jomblo ya?', 'kasur, bantal, dan guling anime', 1, 200),
-(2, 'double', 'berduaan', 'coming soon', 2, 300),
-(3, 'family', 'untuk keluarga sah', 'coming soon', 5, 600);
+(1, 'single', 'Tenda ukuran kecil yang dirancang untuk solo traveler atau tamu yang ingin menikmati ketenangan pribadi. Lokasinya ditempatkan di area yang lebih private dan dekat dengan titik sunrise/sunset.', '•⁠  ⁠1 sleeping pad atau matras single\r\n•⁠  ⁠1 sleeping bag\r\n•⁠  ⁠Lampu tenda LED\r\n•⁠  ⁠Meja kecil lipat\r\n•⁠  ⁠Akses area api unggun umum\r\n•⁠  ⁠Free refill air minum\r\n•⁠  ⁠Charging station di area bersama\r\n•⁠  ⁠Toilet & kamar mandi umum\r\n', 1, 150000),
+(2, 'double', 'Tenda medium dengan ruang lebih luas, cocok untuk pasangan, sahabat, atau dua orang yang ingin camping dengan lebih lega. Area tendanya berada dekat jalur utama dan fasilitas umum.', '•⁠  ⁠2 sleeping pad atau 1 matras double\r\n•⁠  ⁠2 sleeping bag\r\n•⁠  ⁠Lampu tenda LED\r\n•⁠  ⁠Meja & kursi camping\r\n•⁠  ⁠Area api unggun bersama\r\n•⁠  ⁠Free refill air minum\r\n•⁠  ⁠Charging station di area bersama\r\n•⁠  ⁠Toilet & kamar mandi umum\r\n', 2, 250000),
+(3, 'family', 'Tenda besar dengan ruang keluarga, ideal untuk rombongan kecil atau keluarga yang ingin menginap dengan lebih nyaman. Tenda ini memiliki ventilasi besar dan area di sekitar tenda cukup luas untuk aktivitas outdoor ringan.', '•⁠  ⁠Matras sesuai kapasitas (queen + single / 4 sleeping pad)\r\n•⁠  ⁠4 sleeping bag\r\n•⁠  ⁠Lampu tenda LED tambahan\r\n•⁠  ⁠Meja piknik + kursi lipat\r\n•⁠  ⁠Area api unggun khusus family\r\n•⁠  ⁠Free refill air minum\r\n•⁠  ⁠Charging station di area bersama\r\n•⁠  ⁠Toilet & kamar mandi umum dengan beberapa shower room\r\n', 4, 400000);
 
 -- --------------------------------------------------------
 
@@ -153,6 +93,66 @@ CREATE TABLE `pemesanan_master` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tent`
+--
+
+CREATE TABLE `tent` (
+  `tent_id` int(11) UNSIGNED NOT NULL,
+  `paket_id` int(11) UNSIGNED NOT NULL,
+  `nomor_tent` varchar(10) NOT NULL,
+  `nomor_loker` varchar(10) NOT NULL,
+  `status` enum('tersedia','tidak tersedia') NOT NULL DEFAULT 'tersedia'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tent`
+--
+
+INSERT INTO `tent` (`tent_id`, `paket_id`, `nomor_tent`, `nomor_loker`, `status`) VALUES
+(1, 1, 'S01', 'LS01', 'tersedia'),
+(2, 1, 'S02', 'LS02', 'tersedia'),
+(3, 1, 'S03', 'LS03', 'tersedia'),
+(4, 1, 'S04', 'LS04', 'tersedia'),
+(5, 1, 'S05', 'LS05', 'tersedia'),
+(6, 1, 'S06', 'LS06', 'tersedia'),
+(7, 1, 'S07', 'LS07', 'tersedia'),
+(8, 1, 'S08', 'LS08', 'tersedia'),
+(9, 1, 'S09', 'LS09', 'tersedia'),
+(10, 1, 'S10', 'LS10', 'tersedia'),
+(11, 2, 'D01', 'LD01', 'tersedia'),
+(12, 2, 'D02', 'LD02', 'tersedia'),
+(13, 2, 'D03', 'LD03', 'tersedia'),
+(14, 2, 'D04', 'LD04', 'tersedia'),
+(15, 2, 'D05', 'LD05', 'tersedia'),
+(16, 2, 'D06', 'LD06', 'tersedia'),
+(17, 2, 'D07', 'LD07', 'tersedia'),
+(18, 2, 'D08', 'LD08', 'tersedia'),
+(19, 2, 'D09', 'LD09', 'tersedia'),
+(20, 2, 'D10', 'LD10', 'tersedia'),
+(21, 3, 'F01', 'LF01', 'tersedia'),
+(22, 3, 'F02', 'LF02', 'tersedia'),
+(23, 3, 'F03', 'LF03', 'tersedia'),
+(24, 3, 'F04', 'LF04', 'tersedia'),
+(25, 3, 'F05', 'LF05', 'tersedia'),
+(26, 3, 'F06', 'LF06', 'tersedia'),
+(27, 3, 'F07', 'LF07', 'tersedia'),
+(28, 3, 'F08', 'LF08', 'tersedia'),
+(29, 3, 'F09', 'LF09', 'tersedia'),
+(30, 3, 'F10', 'LF10', 'tersedia'),
+(31, 3, 'F11', 'LF11', 'tersedia'),
+(32, 3, 'F12', 'LF12', 'tersedia'),
+(33, 3, 'F13', 'LF13', 'tersedia'),
+(34, 3, 'F14', 'LF14', 'tersedia'),
+(35, 3, 'F15', 'LF15', 'tersedia'),
+(36, 3, 'F16', 'LF16', 'tersedia'),
+(37, 3, 'F17', 'LF17', 'tersedia'),
+(38, 3, 'F18', 'LF18', 'tersedia'),
+(39, 3, 'F19', 'LF19', 'tersedia'),
+(40, 3, 'F20', 'LF20', 'tersedia');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
@@ -166,23 +166,8 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`user_id`, `nama`, `email`, `password`, `role`, `created_at`) VALUES
-(1, 'admin1', 'blmtau@gmail.com', 'apahayo123', 'admin', '2025-11-18 07:10:00'),
-(2, 'tukang tes', 'testing@gmail.com', 'test123', 'user', '2025-11-18 07:10:00');
-
---
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `camp`
---
-ALTER TABLE `camp`
-  ADD PRIMARY KEY (`camp_id`),
-  ADD KEY `fk_camp_paket` (`paket_id`);
 
 --
 -- Indexes for table `detail_pemesanan`
@@ -190,7 +175,7 @@ ALTER TABLE `camp`
 ALTER TABLE `detail_pemesanan`
   ADD PRIMARY KEY (`detail_id`),
   ADD KEY `fk_detail_pemesanan_master` (`pemesanan_id`),
-  ADD KEY `fk_detail_camp` (`camp_id`);
+  ADD KEY `fk_detail_tent` (`tent_id`);
 
 --
 -- Indexes for table `paket`
@@ -213,6 +198,13 @@ ALTER TABLE `pemesanan_master`
   ADD KEY `fk_pemesanan_user` (`user_id`);
 
 --
+-- Indexes for table `tent`
+--
+ALTER TABLE `tent`
+  ADD PRIMARY KEY (`tent_id`),
+  ADD KEY `fk_camp_paket` (`paket_id`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -222,12 +214,6 @@ ALTER TABLE `user`
 --
 -- AUTO_INCREMENT for dumped tables
 --
-
---
--- AUTO_INCREMENT for table `camp`
---
-ALTER TABLE `camp`
-  MODIFY `camp_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
 -- AUTO_INCREMENT for table `detail_pemesanan`
@@ -254,27 +240,27 @@ ALTER TABLE `pemesanan_master`
   MODIFY `pemesanan_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `tent`
+--
+ALTER TABLE `tent`
+  MODIFY `tent_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `user_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `camp`
---
-ALTER TABLE `camp`
-  ADD CONSTRAINT `fk_camp_paket` FOREIGN KEY (`paket_id`) REFERENCES `paket` (`paket_id`) ON UPDATE CASCADE;
-
---
 -- Constraints for table `detail_pemesanan`
 --
 ALTER TABLE `detail_pemesanan`
-  ADD CONSTRAINT `fk_detail_camp` FOREIGN KEY (`camp_id`) REFERENCES `camp` (`camp_id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_detail_pemesanan_master` FOREIGN KEY (`pemesanan_id`) REFERENCES `pemesanan_master` (`pemesanan_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_detail_pemesanan_master` FOREIGN KEY (`pemesanan_id`) REFERENCES `pemesanan_master` (`pemesanan_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_detail_tent` FOREIGN KEY (`tent_id`) REFERENCES `tent` (`tent_id`) ON UPDATE CASCADE;
 
 --
 -- Constraints for table `pembayaran`
@@ -287,6 +273,12 @@ ALTER TABLE `pembayaran`
 --
 ALTER TABLE `pemesanan_master`
   ADD CONSTRAINT `fk_pemesanan_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tent`
+--
+ALTER TABLE `tent`
+  ADD CONSTRAINT `fk_camp_paket` FOREIGN KEY (`paket_id`) REFERENCES `paket` (`paket_id`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
